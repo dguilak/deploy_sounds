0667edce7d0cf953305345e8958b5aa600a4dbc2
Daniel Guilak <daniel@learnup.com>
2016-02-19 16:07:59 -0800

    Play from `~/.deploy_sounds/`

--- lib/deploy_sounds.rb
+++ lib/deploy_sounds.rb
@@ -1,9 +1,15 @@
+# This module inspired by https://github.com/nodanaonlyzuul/Paul-Dix--Thundergod
 module DeploySounds
-  COMMANDS_TO_TRY = %w[afplay play]
+  AUDIO_PLAY_COMMANDS = %w(afplay play)
 
   def self.play
-    COMMANDS_TO_TRY.each do |command|
-      random_file = Dir.glob("./lib/sounds/*").sample
+    AUDIO_PLAY_COMMANDS.each do |command|
+      random_file = Dir.glob("#{ENV['HOME']}/.deploy_sounds/*").sample
+
+      unless random_file
+        puts "[deploy_sounds] You don't have a ~/.deploy_sounds or it's empty."
+        break
+      end
 
       if system("which #{command}")
         `#{command} #{random_file} &`
